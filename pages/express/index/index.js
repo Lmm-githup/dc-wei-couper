dd186e01d0fb3aae3d390e782b8e0363b6baa948
Lmm-githup <18701623984@163.com>
2019-12-18 16:35:20 +0800

    前端图标的稍作修改

--- pages/express/index/index.js
+++ pages/express/index/index.js
@@ -5,7 +5,8 @@ Page({
   goDetail: function(){
     wx.navigateToMiniProgram({
       // appId:'wxfba1f2a0082ea7c9',
-      appId: 'wxc886aa55acc9989a',
+      appId: 'wx9ac6a4ae3519cff4',//德成的微商城
+      // appId: 'wxc886aa55acc9989a',//这是康百家的
       path:'pages/index/index',
       extraData:{},
       envVersion:'release',
